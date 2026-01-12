2c75a96bbdce055d046e4f33c9ef39bea3aa16c9
tiltonk <2983945+Tilton53@users.noreply.github.com>
2026-01-11 23:55:09 -0500

    Testing Layout

--- src/components/flows/solarToBattery.ts
+++ src/components/flows/solarToBattery.ts
@@ -4,7 +4,6 @@ import { showLine } from "@/utils/showLine";
 import { html, svg } from "lit";
 import { styleLine } from "@/utils/styleLine";
 import { type Flows } from "./index";
-import { checkHasBottomIndividual, checkHasRightIndividual } from "@/utils/computeIndividualPosition";
 import { checkShouldShowDots } from "@/utils/checkShouldShowDots";
 
 type FlowSolarToBatteryFlows = Pick<Flows, Exclude<keyof Flows, "grid">>;
@@ -13,9 +12,7 @@ export const flowSolarToBattery = (config: PowerFlowCardPlusConfig, { battery, i
   return battery.has && solar.has && showLine(config, solar.state.toBattery || 0)
     ? html`<div
         class="lines ${classMap({
-          high: battery.has || checkHasBottomIndividual(individual),
-          "individual1-individual2": !battery.has && individual.every((i) => i?.has),
-          "multi-individual": checkHasRightIndividual(individual),
+          high: battery.has,
         })}"
       >
         <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" id="solar-battery-flow" class="flat-line">
--- src/components/flows/solarToHome.ts
+++ src/components/flows/solarToHome.ts
@@ -4,16 +4,13 @@ import { showLine } from "@/utils/showLine";
 import { html, svg } from "lit";
 import { styleLine } from "@/utils/styleLine";
 import { type Flows } from "./index";
-import { checkHasBottomIndividual, checkHasRightIndividual } from "@/utils/computeIndividualPosition";
 import { checkShouldShowDots } from "@/utils/checkShouldShowDots";
 
 export const flowSolarToHome = (config: PowerFlowCardPlusConfig, { battery, grid, individual, solar, newDur }: Flows) => {
   return solar.has && showLine(config, solar.state.toHome || 0) && !config.entities.home?.hide
     ? html`<div
         class="lines ${classMap({
-          high: battery.has || checkHasBottomIndividual(individual),
-          "individual1-individual2": !battery.has && individual.every((i) => i?.has),
-          "multi-individual": checkHasRightIndividual(individual),
+          high: battery.has,
         })}"
       >
         <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" id="solar-home-flow">
